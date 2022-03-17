4f6560df4e26e1ec69c55a4864d0f476a79a8690
KevinJayke <kevin.jayat@gmail.com>
2022-03-17 17:21:17 +0100

    fix: add 1 map extra in the forecast part of the app

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -11,7 +11,7 @@ const Forecast = () => {
 
   return (
     <Grid templateColumns="repeat(auto-fit, minmax(120px, 1fr))" gap={4}>
-      {daysForecasts.slice(0, 4).map(forecast => (
+      {daysForecasts.slice(0, 5).map(forecast => (
         <ForecastCard key={forecast.id} forecast={forecast} />
       ))}
     </Grid>
